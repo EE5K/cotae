62598bab4253231698ef381ed5c9464784b136e0
yo-ong <doorumi13@gmail.com>
2025-05-25 11:33:41 +0900

    update 1838

---
+++ 강동현/1838.js
@@ -0,0 +1,26 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split(/\s+/)
+  .map((el) => Number(el));
+
+input.shift();
+const n = input.length;
+
+for (i = 0; i < n - 1; i++) {
+  flag = 0;
+  for (j = 0; j < n - i - 1; j++) {
+    if (input[j] > input[j + 1]) {
+      flag = 1;
+      temp = input[j];
+      input[j] = input[j + 1];
+      input[j + 1] = temp;
+    }
+  }
+  if (flag == 0) {
+    break;
+  }
+}
+
+console.log(i);
